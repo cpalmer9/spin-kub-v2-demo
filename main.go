4296b49de2394bcabe96477cca1de2c471cd32f8
Christopher Palmer <cpalmer9@yahoo.com>
2019-05-06 22:00:05 -0700

    main.go modify index.html

--- main.go
+++ main.go
@@ -21,6 +21,28 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    thishost, err := os.Hostname()
+    if err != nil {
+        fmt.Printf("Couldn't get Hostname: %v", err);
+    }
+    fmt.Println("hostname:", thishost)
+    input, err := ioutil.ReadFile("/content/index.html")
+    if err != nil {
+        fmt.Printf("Couldn't read index.html: %v", err);
+    }
+
+    lines := strings.Split(string(input), "\n")
+
+    for i, line := range lines {
+        if strings.Contains(line, "    <h2>__HOSTNAME__</h2>") {
+             lines[i] = "    <h2>" + thishost + "</h2>"
+        }
+    }
+    output := strings.Join(lines, "\n")
+    err = ioutil.WriteFile("/content/index.html", []byte(output), 0644)
+    if err != nil {
+        fmt.Printf("Error writing /content/index.html: %v", err);
+    }
 	http.HandleFunc("/", index)
 	port := ":8000"
 	fmt.Printf("Starting to service on port %s\n", port);
